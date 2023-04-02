1f19e414e4c6abece03113899db04ba478c3d44e
Dave Gulati <daveagulati@gmail.com>
2023-04-02 21:36:48 +1000

    Upload build folder

---
+++ build/precache-manifest.ac16423a458a31b35d063f0c6aaca584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca2747738e2b40914cec",
+    "url": "/currencio/static/css/main.7cbf588a.chunk.css"
+  },
+  {
+    "revision": "ca2747738e2b40914cec",
+    "url": "/currencio/static/js/main.1170ce50.chunk.js"
+  },
+  {
+    "revision": "51f6f6309f04dfaeb9f9",
+    "url": "/currencio/static/js/runtime~main.044c3b13.js"
+  },
+  {
+    "revision": "999c10d60ad850a1a8ca",
+    "url": "/currencio/static/js/2.15442a32.chunk.js"
+  },
+  {
+    "revision": "3c31d2a60ee18817aa783d319e31f1a0",
+    "url": "/currencio/static/media/background-min.3c31d2a6.png"
+  },
+  {
+    "revision": "523144de69d6d7e6d6c46ed8502ff6c8",
+    "url": "/currencio/index.html"
+  }
+];
